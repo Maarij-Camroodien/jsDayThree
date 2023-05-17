d78bf56ecc46a31669e6a82a8292be5ea50fa604
Maarij-Camroodien <maarijcamroodien18@gmail.com>
2023-05-17 10:20:23 +0200

    first push

---
+++ script/code.js
@@ -0,0 +1,19 @@
+// String
+let firstName = 'Maarij';
+console.log(typeof firstName);
+
+// Number
+let age = 35
+console.log(typeof age);
+
+// Boolean
+let isAccepted = true;
+console.log(typeof isAccepted);
+
+// undefined
+let surname;
+console.log(typeof surname)
+
+// null
+let color = null;
+console.log(typeof color)
